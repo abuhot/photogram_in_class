78ab84c2b8186108feaab682b08a725465e1a937
Raghu Betina <rbetina@github.com>
2015-07-29 18:31:12 -0500

    Completed show and destroy. Starting on new_form

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,18 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    # params = {"id"=>"4"}
+
+    @photo = Photo.find(params["id"])
+  end
+
+  def destroy
+    @photo = Photo.find(params["id"])
+
+    @photo.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
 
 end
