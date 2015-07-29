9dcee01a6ec063e101f450b6333b5cb0d31b114a
Raghu Betina <rbetina@github.com>
2015-07-29 18:52:40 -0500

    Completes CREATE

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -17,4 +17,29 @@ class PhotosController < ApplicationController
     redirect_to("http://localhost:3000/photos")
   end
 
+  def new_form
+    render("new_form.html.erb")
+  end
+
+  def create_row
+    # params = {"the_caption"=>"A caption", "the_source"=>"some-url"}
+
+    p = Photo.new
+    p.caption = params["the_caption"]
+    p.source = params["the_source"]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+
 end
+
+
+
+
+
+
+
+
+
